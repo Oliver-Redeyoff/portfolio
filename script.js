1eefa88c33e072a56b227f6f086218df147fad09
Oliver <oliver.redeyoff@gmail.com>
2020-04-05 22:10:36 +0100

    Updated mobile style

--- script.js
+++ script.js
@@ -4,26 +4,28 @@ function showHome(){
   var body = document.getElementsByClassName("body")
   var source = `
   <h1>About me</h1>
-  <div id="row">
-    <div id="image">
-      <img src="Assets/terminal.png"/>
+  <div id="aboutMe">
+    <div id="row">
+      <div id="image">
+        <img src="Assets/terminal.png"/>
+      </div>
+      <div id="spacer"></div>
+      <div id="text">
+        <p>I am a Computer-Science student, currently studying at the Univeristy
+        of Bath. I am passionate about my subject and love to further my knowledge through
+        pratical projects and research.</p>
+      </div>
     </div>
-    <div id="spacer"></div>
-    <div id="text">
-      <p>I am a Computer-Science student, currently studying at the Univeristy
-      of Bath. I am passionate about my subject and love to further my knowledge through
-      pratical projects and research.</p>
-    </div>
-  </div>
 
-  <div id="row">
-    <div id="text">
-      <p>I am currently completing the final year of my degree and am looking
-        for a graduate position. You can view my work <a>here</a>, and can contact me <a>here</a>. </p>
-    </div>
-    <div id="spacer"></div>
-    <div id="image">
-      <img src="Assets/resume.png"/>
+    <div id="row">
+      <div id="text">
+        <p>I am currently completing the final year of my degree and am looking
+          for a graduate position. You can view my work <a>here</a>, and can contact me <a>here</a>. </p>
+      </div>
+      <div id="spacer"></div>
+      <div id="image">
+        <img src="Assets/resume.png"/>
+      </div>
     </div>
   </div>
   `
@@ -39,26 +41,28 @@ function showMyWork(){
   var body = document.getElementsByClassName("body")
   var source = `
   <h1>My Work</h1>
-  <div id="row">
-    <div id="image">
-      <img src="Assets/terminal.png"/>
+  <div id="aboutMe">
+    <div id="row">
+      <div id="image">
+        <img src="Assets/terminal.png"/>
+      </div>
+      <div id="spacer"></div>
+      <div id="text">
+        <p>I am a Computer-Science student, currently studying at the Univeristy
+        of Bath. I am passionate about my subject and love to further my knowledge through
+        pratical projects and research.</p>
+      </div>
     </div>
-    <div id="spacer"></div>
-    <div id="text">
-      <p>I am a Computer-Science student, currently studying at the Univeristy
-      of Bath. I am passionate about my subject and love to further my knowledge through
-      pratical projects and research.</p>
-    </div>
-  </div>
 
-  <div id="row">
-    <div id="text">
-      <p>I am currently completing the final year of my degree and am looking
-        for a graduate position. You can view my work <a>here</a>, and can contact me <a>here</a>. </p>
-    </div>
-    <div id="spacer"></div>
-    <div id="image">
-      <img src="Assets/resume.png"/>
+    <div id="row">
+      <div id="text">
+        <p>I am currently completing the final year of my degree and am looking
+          for a graduate position. You can view my work <a>here</a>, and can contact me <a>here</a>. </p>
+      </div>
+      <div id="spacer"></div>
+      <div id="image">
+        <img src="Assets/resume.png"/>
+      </div>
     </div>
   </div>
   `
